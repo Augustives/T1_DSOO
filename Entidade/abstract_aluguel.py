c00d4e588180c80b4fb9e998d1f476d49e49e700
Augustives <augusto1408@hotmail.com>
2019-10-03 14:11:43 -0300

    entidades abstratas

--- Entidade/abstract_aluguel.py
+++ Entidade/abstract_aluguel.py
@@ -3,9 +3,11 @@ from datetime import date as Date
 
 class AbstractAluguel(ABC):
 
-
-@abstractmethod
-def __init__(self, pessoa: Pessoa, quadra: Quadra, data_hora: str):
-# https://stackabuse.com/converting-strings-to-datetime-in-python/
+    @abstractmethod
+    def __init__(self, pessoa: Pessoa, quadra: Quadra, data_str: str):
+    # https://stackabuse.com/converting-strings-to-datetime-in-python/
+        self.__pessoa = pessoa
+        self.__quadra = quadra
+        self.__data_str = data_str
 
 
---
+++ Entidade/quadra.py
@@ -0,0 +1,38 @@
+from Entidade.abstract_quadra import AbstractQuadra
+
+class Quadra(AbstractQuadra):
+    def __init__(self, esporte: str, tipo: str, identificador: int):
+        super().__init__(esporte: str, tipo: str, identificador: int)
+        self.__esporte = esporte
+        self.__tipo = tipo
+        self.__identificador = identificador
+
+    @property
+    def esporte(self):
+        return self.__esporte
+
+
+    @esporte.setter
+    def esporte(self, esporte):
+        self.__esporte = esporte
+
+
+    @property
+    def tipo(self):
+        return self.__tipo
+
+
+    @tipo.setter
+    def tipo(self, tipo):
+        self.__tipo = tipo
+
+
+    @property
+    def identificador(self):
+        return self.__identificador
+
+
+    @identificador.setter
+    def identificador(self, identificador):
+        self.__identificador = identificador
+
